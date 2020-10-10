7791bffcec5b07269b8f1ff2f6602c07d392c7b7
Lukáš Trkan <lukas@trkan.cz>
2020-10-10 15:14:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 CESTA=/usr/local/sbin
-curl -o "$CESTA"/pogtest_check "https://raw.githubusercontent.com/lukastrkan/pogtest_scripty/main/pogtest_check"
-chmod +x "$CESTA"/pogtest_check;
-curl -o "$CESTA"/pogtest_compile "https://raw.githubusercontent.com/lukastrkan/pogtest_scripty/main/pogtest_compile"
-chmod +x "$CESTA"/pogtest_compile;
-curl -o "$CESTA"/pogtest_run "https://raw.githubusercontent.com/lukastrkan/pogtest_scripty/main/pogtest_run";
-chmod +x "$CESTA"/pogtest_run;
+sudo curl -o "$CESTA"/pogtest_check "https://raw.githubusercontent.com/lukastrkan/pogtest_scripty/main/pogtest_check"
+sudo chmod +x "$CESTA"/pogtest_check;
+sudo curl -o "$CESTA"/pogtest_compile "https://raw.githubusercontent.com/lukastrkan/pogtest_scripty/main/pogtest_compile"
+sudo chmod +x "$CESTA"/pogtest_compile;
+sudo curl -o "$CESTA"/pogtest_run "https://raw.githubusercontent.com/lukastrkan/pogtest_scripty/main/pogtest_run";
+sudo chmod +x "$CESTA"/pogtest_run;
